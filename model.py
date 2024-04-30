e5ef9f521d5815bace5ca2c2293ae5b10c3e07ed
Hilal Mufti <hmufti@cs.washington.edu>
2024-04-30 13:02:48 -0700

    +datasets, notebooks

--- model.py
+++ model.py
@@ -3,24 +3,8 @@ from dataclasses import dataclass
 import equinox as eqx 
 import jax 
 
-@dataclass 
-class GPTConfig: 
-  block_size: int = 1024
-  vocab_size: int = 50304 
-  n_layer: int = 12
-  n_head: int = 12
-  n_embd: int = 768
-  dropout: float = 0.0
-  bias: bool = True
 
-class GPT(eqx.Module): 
-  weight: jax.Array
-  bias: jax.Array
 
-  def __init__(self): 
-    pass 
 
-  def __call__(self, x): 
-    return x
 
   
