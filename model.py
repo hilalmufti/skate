aebf8a0db14ade0f15b6a171e720473c940285c4
Hilal Mufti <hmufti@cs.washington.edu>
2024-05-01 23:52:21 -0700

    +self attention block

--- model.py
+++ model.py
@@ -1,9 +1,22 @@
 from dataclasses import dataclass
 
 import equinox as eqx 
+import eqx.nn as nn
 import jax 
 
+class Head(eqx.Module):
+  key: eqx.Module 
+  query: eqx.Module
+  value: eqx.Module
+  head_size: int
 
+  def __init__(self, head_size): 
+    self.key = nn.Linear(n_embd, head_size, use_bias=False)
+    self.query = nn.Linear(n_embd, head_size, use_bias=False)
+    self.value = nn.Linear(n_embd, head_size, use_bias=False)
+
+  def __call__(self, x):
+    B, T, C = x.shape
 
 
 
